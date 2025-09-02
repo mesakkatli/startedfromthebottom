c351971aa8f759f72d23f2cb6e31b534d095d386
mesakkatli <2024111068@ogr.cu.edu.tr>
2025-09-03 02:36:30 +0300

    Add files via upload

---
+++ veri.js
@@ -0,0 +1,300 @@
+const dersVeritabani = {
+    "donem1": {
+        "biyokimya": {
+            "dersAdi": "Dönem 1 - Tıbbi Biyokimya",
+            "konular": [
+                "Biyokimyaya Giriş ve Temel Kavramlar",
+                "Hücre Biyokimyası: Suyun yapısı, pH, tampon sistemler",
+                "Amino Asitler ve Proteinlerin Yapı ve Fonksiyonları",
+                "Enzimler: Kinetik, sınıflandırma ve düzenlenme",
+                "Karbonhidratların Yapısı ve Metabolizmasına Giriş",
+                "Lipitlerin Yapısı ve Sınıflandırılması",
+                "Nükleik Asitler (DNA ve RNA): Yapı ve fonksiyonları"
+            ]
+        },
+        "histoloji": {
+            "dersAdi": "Dönem 1 - Histoloji ve Embriyoloji",
+            "konular": [
+                "Histolojiye Giriş ve Mikroskopi Teknikleri",
+                "Hücre Yapısı",
+                "Temel Dokular: Epitel, bağ, kıkırdak, kemik, kas, sinir",
+                "Gametogenez (Sperm ve oosit oluşumu)",
+                "Fertilizasyon (Döllenme) ve ilk hafta gelişimi",
+                "İkinci ve üçüncü hafta gelişimi: Germ yapraklarının oluşumu"
+            ]
+        },
+        "cocuk": {
+            "dersAdi": "Dönem 1 - Çocuk Sağlığı ve Hastalıkları (Temel)",
+            "konular": [
+                "Büyüme ve Gelişme Temelleri",
+                "Temel Beslenme",
+                "Aşıların Önemi (Giriş)"
+            ]
+        },
+        "biyoloji": {
+            "dersAdi": "Dönem 1 - Tıbbi Biyoloji",
+            "konular": [
+                "Hücre Biyolojisi: Organellerin yapısı ve işlevleri",
+                "Hücre Bölünmesi: Mitoz ve mayoz",
+                "Moleküler Biyolojiye Giriş: DNA replikasyonu, transkripsiyon, translasyon",
+                "Genetik Kavramlar: Mendel genetiği, genetik hastalıkların temelleri",
+                "Hücre İskeleti ve Hücre Hareketleri",
+                "Hücreler Arası İletişim"
+            ]
+        },
+        "fizyoloji": {
+            "dersAdi": "Dönem 1 - Fizyoloji",
+            "konular": [
+                "Fizyolojiye Giriş ve Homeostaz",
+                "Hücre Fizyolojisi: Membran potansiyelleri",
+                "Kas Fizyolojisi",
+                "Kan Fizyolojisi",
+                "Sinir Sistemi Fizyolojisine Giriş"
+            ]
+        },
+        "biyofizik": {
+            "dersAdi": "Dönem 1 - Biyofizik",
+            "konular": [
+                "Biyofiziğe Giriş",
+                "Biyolojik Sistemlerde Termodinamik",
+                "Membran Biyofiziği: Madde taşınması",
+                "Biyoelektrik ve Biyomanyetizma (EKG, EEG, EMG temelleri)",
+                "Biyomekanik",
+                "Ses ve İşitme Biyofiziği",
+                "Işık ve Görme Biyofiziği",
+                "Radyasyon Biyofiziği ve Tıbbi Görüntüleme Prensipleri"
+            ]
+        },
+        "anatomi": {
+            "dersAdi": "Dönem 1 - Anatomi",
+            "konular": [
+                "Anatomiye Giriş ve Terminoloji",
+                "Hareket Sistemi Anatomisi: Kemikler, Eklemler, Kaslar",
+                "Üst ve Alt Ekstremite Anatomisi (Kol ve bacak yapıları)",
+                "Gövde Anatomisi: Sırt, göğüs duvarı ve karın duvarı"
+            ]
+        },
+        "ruh": {
+            "dersAdi": "Dönem 1 - Ruh Sağlığı (Temel)",
+            "konular": [
+                "Tıbbi Etik ve Davranış Bilimleri",
+                "Hasta-Hekim İlişkisi",
+                "İletişim Becerileri"
+            ]
+        },
+        "halk": {
+            "dersAdi": "Dönem 1 - Halk Sağlığı",
+            "konular": [
+                "Halk Sağlığına Giriş, Temel Kavramlar",
+                "Demografi ve Sağlık",
+                "Epidemiyolojiye Giriş",
+                "Sağlık Eğitimi ve Sağlığı Geliştirme"
+            ]
+        }
+    },
+    "donem2": {
+        "biyokimya": {
+            "dersAdi": "Dönem 2 - Tıbbi Biyokimya",
+            "konular": [
+                "Metabolizma: Karbonhidrat, lipit, protein ve nükleotid metabolizması",
+                "Hormonlar ve Sinyal İletimi",
+                "Metabolik Hastalıkların Biyokimyasal Temelleri",
+                "Vitaminler ve Minerallerin Biyokimyası",
+                "Kan Biyokimyası: Plazma proteinleri, pıhtılaşma",
+                "Karaciğer ve Böbrek Fonksiyon Testleri"
+            ]
+        },
+        "histoloji": {
+            "dersAdi": "Dönem 2 - Histoloji ve Embriyoloji",
+            "konular": [
+                "Dolaşım Sistemi Histolojisi",
+                "Solunum Sistemi Histolojisi",
+                "Sindirim Sistemi Histolojisi",
+                "Üriner Sistem Histolojisi",
+                "Endokrin Sistem Histolojisi",
+                "Erkek ve Kadın Genital Sistem Histolojisi",
+                "Duyu Organları Histolojisi",
+                "Organogenez: Organ ve sistemlerin gelişimi",
+                "Doğumsal Anomaliler"
+            ]
+        },
+        "cocuk": {
+            "dersAdi": "Dönem 2 - Çocuk Sağlığı ve Hastalıkları (Temel)",
+            "konular": [
+                "Büyüme-Gelişme",
+                "Beslenme",
+                "Aşılar"
+            ]
+        },
+        "fizyoloji": {
+            "dersAdi": "Dönem 2 - Fizyoloji",
+            "konular": [
+                "Kardiyovasküler Sistem Fizyolojisi",
+                "Solunum Sistemi Fizyolojisi",
+                "Böbrek (Üriner Sistem) Fizyolojisi",
+                "Sindirim Sistemi Fizyolojisi",
+                "Endokrin Sistem Fizyolojisi",
+                "Üreme Sistemi Fizyolojisi"
+            ]
+        },
+        "anatomi": {
+            "dersAdi": "Dönem 2 - Anatomi",
+            "konular": [
+                "Dolaşım Sistemi Anatomisi (Kalp, arterler, venler)",
+                "Solunum Sistemi Anatomisi",
+                "Sindirim Sistemi Anatomisi",
+                "Üriner ve Genital Sistem Anatomisi (Ürogenital Sistem)",
+                "Endokrin Sistem Anatomisi"
+            ]
+        },
+        "ruh": {
+            "dersAdi": "Dönem 2 - Ruh Sağlığı (Temel)",
+            "konular": [
+                "Tıbbi Etik ve Davranış Bilimleri",
+                "Hasta-Hekim İlişkisi",
+                "İletişim Becerileri"
+            ]
+        },
+        "halk": {
+            "dersAdi": "Dönem 2 - Halk Sağlığı",
+            "konular": [
+                "Halk Sağlığına Giriş, Temel Kavramlar",
+                "Demografi ve Sağlık",
+                "Epidemiyolojiye Giriş",
+                "Sağlık Eğitimi ve Sağlığı Geliştirme"
+            ]
+        }
+    },
+    "donem3": {
+        "biyokimya": {
+            "dersAdi": "Dönem 3 - Tıbbi Biyokimya",
+            "konular": [
+                "Klinik Biyokimya: Hastalıklarda görülen biyokimyasal değişiklikler",
+                "Spesifik Sistem Hastalıklarının Biyokimyası (Kardiyovasküler, Endokrin vb.)",
+                "Kanser Biyokimyası",
+                "Moleküler Biyoloji Tekniklerine Giriş"
+            ]
+        },
+        "fizyoloji": {
+            "dersAdi": "Dönem 3 - Fizyoloji",
+            "konular": [
+                "İleri Nörofizyoloji (Beyin, duyu organları, otonom sinir sistemi)",
+                "Patofizyoloji: Hastalıkların fizyolojik temelleri"
+            ]
+        },
+        "anatomi": {
+            "dersAdi": "Dönem 3 - Anatomi",
+            "konular": [
+                "Nöroanatomi: Merkezi ve Periferik Sinir Sistemi",
+                "Baş ve Boyun Anatomisi",
+                "Duyu Organları Anatomisi",
+                "Topografik ve Klinik Anatomi"
+            ]
+        },
+        "ruh": {
+            "dersAdi": "Dönem 3 - Ruh Sağlığı ve Hastalıkları",
+            "konular": [
+                "Psikopatolojiye Giriş",
+                "Semptomlar ve Sendromlar: Duygudurum, anksiyete, psikoz",
+                "Psikiyatrik Muayene ve Görüşme Teknikleri",
+                "Psikiyatrik Hastalıkların Sınıflandırılması"
+            ]
+        },
+        "halk": {
+            "dersAdi": "Dönem 3 - Halk Sağlığı",
+            "konular": [
+                "Bulaşıcı Hastalıklar Epidemiyolojisi ve Kontrolü",
+                "Bulaşıcı Olmayan Hastalıklar ve Risk Faktörleri",
+                "Çevre Sağlığı, İş Sağlığı",
+                "Beslenme ve Toplum Beslenmesi",
+                "Aşılamanın Toplumsal Önemi"
+            ]
+        }
+    },
+    "donem4": {
+        "cocuk": {
+            "dersAdi": "Dönem 4 - Çocuk Sağlığı ve Hastalıkları (Staj)",
+            "konular": [
+                "Sağlam Çocuk İzlemi",
+                "Yenidoğan Hastalıkları",
+                "Çocuk Enfeksiyon Hastalıkları",
+                "Çocuk Acil ve Yoğun Bakım",
+                "Pediatrik Yan Dal Hastalıklarına Giriş",
+                "Genetik ve Metabolik Hastalıklar"
+            ]
+        },
+        "ruh": {
+            "dersAdi": "Dönem 4 - Ruh Sağlığı ve Hastalıkları (Staj)",
+            "konular": [
+                "Duygudurum Bozuklukları (Depresyon, Bipolar)",
+                "Anksiyete Bozuklukları (Panik, OKB)",
+                "Şizofreni ve Diğer Psikotik Bozukluklar",
+                "Alkol ve Madde Kullanım Bozuklukları",
+                "Psikofarmakoloji"
+            ]
+        },
+        "halk": {
+            "dersAdi": "Dönem 4 - Halk Sağlığı (Staj)",
+            "konular": [
+                "Toplum Sağlığı Merkezi (TSM) Çalışmaları",
+                "Aile Sağlığı Merkezi (ASM) Çalışmaları",
+                "Bağışıklama, Aile Planlaması",
+                "Sağlık Yönetimi"
+            ]
+        }
+    },
+    "donem5": {
+        "cocuk": {
+            "dersAdi": "Dönem 5 - Çocuk Sağlığı ve Hastalıkları (Staj)",
+            "konular": [
+                "Sağlam Çocuk İzlemi",
+                "Yenidoğan Hastalıkları",
+                "Çocuk Enfeksiyon Hastalıkları",
+                "Çocuk Acil ve Yoğun Bakım",
+                "Pediatrik Yan Dal Hastalıklarına Giriş",
+                "Genetik ve Metabolik Hastalıklar"
+            ]
+        },
+        "ruh": {
+            "dersAdi": "Dönem 5 - Ruh Sağlığı ve Hastalıkları (Staj)",
+            "konular": [
+                "Duygudurum Bozuklukları (Depresyon, Bipolar)",
+                "Anksiyete Bozuklukları (Panik, OKB)",
+                "Şizofreni ve Diğer Psikotik Bozukluklar",
+                "Alkol ve Madde Kullanım Bozuklukları",
+                "Psikofarmakoloji"
+            ]
+        },
+        "halk": {
+            "dersAdi": "Dönem 5 - Halk Sağlığı (Staj)",
+            "konular": [
+                "Toplum Sağlığı Merkezi (TSM) Çalışmaları",
+                "Aile Sağlığı Merkezi (ASM) Çalışmaları",
+                "Bağışıklama, Aile Planlaması",
+                "Sağlık Yönetimi"
+            ]
+        }
+    },
+    "donem6": {
+        "cocuk": {
+            "dersAdi": "Dönem 6 - Çocuk Sağlığı ve Hastalıkları (İntörnlük)",
+            "konular": [
+                "Pediatri Servisleri ve Polikliniklerinde Aktif Hasta Takibi",
+                "Tanı ve Tedavi Süreçlerine Katılım"
+            ]
+        },
+        "ruh": {
+            "dersAdi": "Dönem 6 - Ruh Sağlığı ve Hastalıkları (İntörnlük)",
+            "konular": [
+                "Psikiyatri Servisi ve Polikliniğinde Hasta Takibi",
+                "Görüşme Yapma ve Tedavi Planına Katılma"
+            ]
+        },
+        "halk": {
+            "dersAdi": "Dönem 6 - Halk Sağlığı (İntörnlük)",
+            "konular": [
+                "Birinci Basamak Sağlık Kuruluşlarında Pratik Uygulamalar"
+            ]
+        }
+    }
+};
